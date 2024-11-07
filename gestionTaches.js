81f7d09e5b847a72d83c99c682d923a2fe04237e
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-11-07 16:06:33 +0100

    save task

--- gestionTaches.js
+++ gestionTaches.js
@@ -1,15 +1,36 @@
 "use strict";
 
-/* ajouter "tache terminer" ( add icon checked)
- * supprimer tache 
- * filtrer taches (toute, active, suppr)
- * sauvegarde locale storage
+/*   filtrer taches (toute, terminer, suppr)
+* sauvegarde locale storage
 */
+
+const tabTasks = [...loadTask()];
+
+// enregistrer le tache dans le locale storage
+function saveTask() {
+    localStorage.setItem("Tasks", JSON.stringify(tabTasks));
+};
+
+//charger les taches depuis le local storage 
+function loadTask() {
+    const storageTask = localStorage.getItem("Tasks");
+   if (storageTask) {
+        return JSON.parse(storageTask)
+   } else {
+        return [];
+   }
+};
+
+// afficher les taches chargé depuis le storage
+function displayTask() {
+    for (let i = 0; i < tabTasks.length; i++) {
+        tabTasks[i] = addTask();
+    }
+};
 
 // recuperer nouvelles tache au click et ajouter tache
 const inputTask = document.getElementsByClassName("task")[0];
 const ulGroup = document.getElementsByClassName("list-group")[0];
-const tabTask = [];
 
 const btnAdd = document.getElementById("button-addon2");
 btnAdd.addEventListener("click", addTask);
@@ -21,35 +42,41 @@ function addTask() {
     ulGroup.appendChild(newListItem);
 
     const newButtonDel = document.createElement("button");
-    newButtonDel.textContent = "Supprimer";
-    newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm", "btn-suppr")
-    newButtonDel.setAttribute("id", "suppr")
+    newButtonDel.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
+    newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm", "btn-suppr");
     newListItem.appendChild(newButtonDel);
 
-    tabTask.push(inputTask.value);
+    tabTasks.push(newListItem.textContent);
+    saveTask();
     inputTask.value = "";
 }
 
+
 // definir tache terminer
 ulGroup.addEventListener("click", finishTask)
+ulGroup.addEventListener("click", deleteTask)
 
 function finishTask(event) {
     const listItem = document.querySelectorAll('li');
     for (let i = 0; i < listItem.length; i++) {
         if (event.target.tagName === "LI") {
-            event.target.classList.add("disabled", "text-decoration-line-trough", "text-danger");
+            event.target.classList.toggle("text-success");
+            
+            tabTasks.push()
+            saveTask();
         }
     }
 };
+console.log(localStorage.getItem("Tasks"))
 
 // supprimer tache
-const btnDelete = document.getElementById("suppr");
-btnDelete.addEventListener("click", (event) => event.target.remove);
-
-
-console.log(tabTask)
-
-localStorage.setItem("tasks", JSON.stringify(tabTask));
-
+function deleteTask(event) {
+    if (event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+        console.log("ok");
+    } else if(event.target.tagName === "I"){
+        event.target.parentElement.parentElement.remove();
+    }
+};
 
 
