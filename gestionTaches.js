612f6414bfbc63aa046fdc129c5bc73823b54c78
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-11-12 15:42:27 +0100

    css/modal

--- gestionTaches.js
+++ gestionTaches.js
@@ -34,17 +34,17 @@ function displayTasks() {
 
         // tache a faire
         if (optionSelect === 1) {
-            if (tabTasks[i].done === false){
+            if (tabTasks[i].done === false) {
                 displayTask(tabTasks[i]);
             }
 
-        // tache terminer  
-        } else if (optionSelect === 2 ) {
-            if(tabTasks[i].done === true){
+            // tache terminer  
+        } else if (optionSelect === 2) {
+            if (tabTasks[i].done === true) {
                 displayTask(tabTasks[i]);
             }
 
-         // toutes les taches    
+            // toutes les taches    
         } else {
             displayTask(tabTasks[i]);
         }
@@ -53,14 +53,14 @@ function displayTasks() {
 
 function displayTask(task) {
     const newListItem = document.createElement("li");
-    newListItem.textContent = task.title; 
+    newListItem.textContent = task.title;
     ulGroup.appendChild(newListItem);
     const newButtonDel = document.createElement("button");
     newButtonDel.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
-    newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm","ms-auto", "btn-suppr");
+    newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm", "ms-auto", "btn-suppr");
     newListItem.appendChild(newButtonDel);
     inputTask.value = "";
-    if (task.done === false){
+    if (task.done === false) {
         newListItem.classList.add("list-group-item", "p-2", "d-flex");
     } else {
         newListItem.classList.add("list-group-item", "p-2", "d-flex", "fst-italic", "text-danger");
@@ -74,18 +74,20 @@ function displayTask(task) {
 de done true ou false */
 
 // recuperer nouvelles tache au click et ajouter tache
-const btnAdd = document.getElementById("button-addon2");
+const btnAdd = document.getElementById("button-add");
 btnAdd.addEventListener("click", addTask);
 inputTask.addEventListener("keypress", keyPress)
 
 function addTask() {
+    const modalBody = document.querySelector(".modal-body")
+    modalBody.textContent = inputTask.value;
     tabTasks.push({ title: inputTask.value, done: false });
     saveTask();
     displayTasks();
 }
 
 function keyPress(event) {
-    if (inputTask.value !== ""){
+    if (inputTask.value !== "") {
         if (event.key === "Enter") {
             addTask();
         } else if (event.key === "NumpadEnter") {
@@ -99,14 +101,17 @@ ulGroup.addEventListener("click", finishTask);
 ulGroup.addEventListener("click", deleteTask);
 
 function finishTask(event) {
-        if (event.target.tagName === "LI") {
-            const targetTaskIndex = tabTasks.findIndex(function (task) {
-                return task.title === event.target.textContent;
-            });     
-            tabTasks[targetTaskIndex].done = true ;
-            saveTask();
-            displayTasks();
-        }
+    if (event.target.tagName === "LI") {
+        const targetTaskIndex = tabTasks.findIndex(function (task) {
+            return task.title === event.target.textContent;
+        });
+        if (tabTasks[targetTaskIndex].done === false) {
+            tabTasks[targetTaskIndex].done = true;
+        } else if (tabTasks[targetTaskIndex].done === true)
+            tabTasks[targetTaskIndex].done = false;
+        saveTask();
+        displayTasks();
+    }
 };
 
 console.log(localStorage.getItem("Tasks"))
@@ -125,5 +130,10 @@ function deleteTask(event) {
         saveTask();
     }
 };
+
+function openModal() {
+    const divModal = document.createElement("div");
+
+}
 
 displayTasks();
