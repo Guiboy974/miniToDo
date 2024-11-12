022e9fbcf2208fe44c9d75894e0ee5993602dc56
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-11-12 14:03:10 +0100

    filterON

--- gestionTaches.js
+++ gestionTaches.js
@@ -23,46 +23,75 @@ const tabTasks = loadTask();
 const inputTask = document.getElementsByClassName("task")[0];
 const ulGroup = document.getElementsByClassName("list-group")[0];
 const selectOption = document.getElementsByTagName("select")[0];
+selectOption.addEventListener("change", displayTasks);
 
 // afficher toutes les taches chargé depuis le storage
 function displayTasks() {
     ulGroup.innerHTML = "";
     for (let i = 0; i < tabTasks.length; i++) {
         // trier les taches en cours et terminer
-        selectOption.addEventListener("change", sortTasks)
-        function sortTasks(event) {
-            const optionSelect = event.target.selectedIndex;
-            console.log(event.target.selectedIndex);
-            
-            if (optionSelect === 1){
-                let tab1 = tabTasks.sort((a, b) => a.done - b.done);
-                console.log(tab1)
-            }
-        }
+        const optionSelect = selectOption.selectedIndex;
 
-        
-        const newListItem = document.createElement("li");
-        newListItem.classList.add("list-group-item", "p-2", "d-flex", "justify-content-between");
-        newListItem.textContent = tabTasks[i].title;
-        ulGroup.appendChild(newListItem);
-        const newButtonDel = document.createElement("button");
-        newButtonDel.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
-        newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm", "btn-suppr");
-        newListItem.appendChild(newButtonDel);
-        inputTask.value = "";
+        // tache a faire
+        if (optionSelect === 1) {
+            if (tabTasks[i].done === false){
+                displayTask(tabTasks[i]);
+            }
+
+        // tache terminer  
+        } else if (optionSelect === 2 ) {
+            if(tabTasks[i].done === true){
+                displayTask(tabTasks[i]);
+            }
+
+         // toutes les taches    
+        } else {
+            displayTask(tabTasks[i]);
+        }
     }
 };
 
+function displayTask(task) {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = task.title; 
+    ulGroup.appendChild(newListItem);
+    const newButtonDel = document.createElement("button");
+    newButtonDel.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
+    newButtonDel.classList.add("btn", "btn-outline-danger", "btn-sm","ms-auto", "btn-suppr");
+    newListItem.appendChild(newButtonDel);
+    inputTask.value = "";
+    if (task.done === false){
+        newListItem.classList.add("list-group-item", "p-2", "d-flex");
+    } else {
+        newListItem.classList.add("list-group-item", "p-2", "d-flex", "fst-italic", "text-danger");
+        const newIcon = document.createElement("i");
+        newIcon.innerHTML = '<i class="bi bi-check-all p-2"></i>';
+        newListItem.insertBefore(newIcon, newButtonDel)
+    }
+}
+
+/*faire un if dans display task pour affiché les classList en fonction de
+de done true ou false */
 
 // recuperer nouvelles tache au click et ajouter tache
-
 const btnAdd = document.getElementById("button-addon2");
 btnAdd.addEventListener("click", addTask);
+inputTask.addEventListener("keypress", keyPress)
 
 function addTask() {
-    tabTasks.push({ title:inputTask.value, done:false });
+    tabTasks.push({ title: inputTask.value, done: false });
     saveTask();
     displayTasks();
+}
+
+function keyPress(event) {
+    if (inputTask.value !== ""){
+        if (event.key === "Enter") {
+            addTask();
+        } else if (event.key === "NumpadEnter") {
+            addTask();
+        }
+    }
 }
 
 // definir tache terminer
@@ -70,16 +99,16 @@ ulGroup.addEventListener("click", finishTask);
 ulGroup.addEventListener("click", deleteTask);
 
 function finishTask(event) {
-    const listItem = document.querySelectorAll('li');
-    for (let i = 0; i < listItem.length; i++) {
         if (event.target.tagName === "LI") {
-            event.target.classList.add("text-danger");
-
-            // tabTasks[i].done=true;
+            const targetTaskIndex = tabTasks.findIndex(function (task) {
+                return task.title === event.target.textContent;
+            });     
+            tabTasks[targetTaskIndex].done = true ;
             saveTask();
+            displayTasks();
         }
-    }
 };
+
 console.log(localStorage.getItem("Tasks"))
 
 // supprimer tache
@@ -87,13 +116,11 @@ function deleteTask(event) {
     if (event.target.tagName === "BUTTON") {
         const index = tabTasks[event.target.parentElement];
         event.target.parentElement.remove();
-
         tabTasks.splice(index, 1);
         saveTask();
     } else if (event.target.tagName === "I") {
         const index2 = tabTasks[event.target.parentElement.parentElement];
         event.target.parentElement.parentElement.remove();
-
         tabTasks.splice(index2, 1);
         saveTask();
     }
